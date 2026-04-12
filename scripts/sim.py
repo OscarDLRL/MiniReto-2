d13b10f422ed803c1ecd488d8f6248cd8e50af85
OscarDLRL <oscardelarosalopez05@gmail.com>
2026-04-12 15:48:44 -0600

    Main movement script

---
+++ scripts/sim.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    print("\n" + "#" * 70)
+    print("#   ROBOTS MOVILES TERRESTRES - Ejecutando todos los demos")
+    print("#" * 70 + "\n")
+
+    demo_puzzlebot()
+    demo_husky()
+    demo_anymal()
+
+    print("\n" + "=" * 70)
+    print("Todos los demos ejecutados. Revisa las imagenes *.png generadas.")
+    print("=" * 70)
+
+    # Mostrar todas las figuras al final
+    plt.show()
